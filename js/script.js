4efc8efb8ef6d1169db7031806f2b4473522973a
Abdullah Zia <amz1cflp99@gmail.com>
2025-01-30 23:37:43 -0500

    Intitial commit for personal website

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+  let welcomeMsg = document.createElement("p");
+  welcomeMsg.textContent = "Thanks for visiting my portfolio!";
+  document.getElementById("home").appendChild(welcomeMsg);
+});
